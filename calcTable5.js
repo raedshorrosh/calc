4d66f2fd84858a000bbfb867a7242cc6533bb534
Raed Shorrosh <90145979+raedshorrosh@users.noreply.github.com>
2024-08-28 21:49:35 +0300

    Update calcTable5.js

--- calcTable5.js
+++ calcTable5.js
@@ -5,18 +5,12 @@
 [[style href="https://rawcdn.githack.com/raedshorrosh/calc/3070ff0e73239c4e5cef044d4cb3a84dd4925fa2/jexcel.css" type="text/css" /]]
 [[style href="https://fonts.googleapis.com/css?family=Material+Icons" type="text/css" /]]
 [[script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_HTMLorMML" /]]
-[[comment]] ver 1.12 [[/comment]]
- <p style=display:none>`x^2`,`x/y`, \(\small C_6H_{12}O_{6(s)}\)</p>
- 
+[[comment]] ver 1.13 show hint button works now [[/comment]]
+  
  <div style="display: flex; justify-content: center; width:100%; font-size:{@fontsize@}">
    <div id="spreadsheet" dir="ltr" ></div>
    <div id="myView" style="display:none;" ></div>
  </div>
-
-                                 
-
- 
-
    
 [[script type="module"]]
 
@@ -157,7 +151,7 @@ nestedHeaders:nested,
          toolbar:toolbar,
   updateTable: function (instance, cell, col, row, val, label, cellName) {
    var isAttemptMode = window.location.href.indexOf('attempt.php') !== -1;
-   console.log('isAttemptMode ',isAttemptMode,'readonly ',readonly); 
+   
    if (isAttemptMode && readonly) {cell.classList.add('readonly');}                                    
    dataInput.value=JSON.stringify(instance.jspreadsheet.getData());dataInput.dispatchEvent(new Event('change'));
    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
@@ -178,7 +172,7 @@ table.onbeforechange= function(instance, cell, x, y, value){if (readonly) {cell.
    var hint_el= document.getElementById(uid_hint);
        hint_el.appendChild(btn);
      btn.disabled=true; 
-              console.log("show the button is ",showHint);
+            
  if ( ({#hint_enable#}==1) || showHint) {hint_el.style.display = "block";btn.disabled = false;}        
 
 
